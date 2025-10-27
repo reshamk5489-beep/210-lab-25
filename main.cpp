30c3f17970f46b7ec4d2f8f41cf73663d701ffb8
Resham <resha@MacBook-Air-2.local>
2025-10-26 22:24:38 -0700

    Working on Race 3 Functions

--- main.cpp
+++ main.cpp
@@ -14,7 +14,9 @@ void printRightJustified(string text, int width);
 int sortVector(vector<string> &codes);
 int sortList(list<string> &codes);
 int sortSet(set<string> &codes);
-
+int insertVector(vector<string> &codesVector, string val);
+int insertList(list<string> &codesList, string val);
+int insertSet(set<string> &codesSet, string val);
 
 int main() {
     vector<string> codesVector;
@@ -33,14 +35,14 @@ int main() {
     printRightJustified(to_string(readSetTime), 10); 
     cout << endl;
 
-    int sortVectorTime = sortVector();
-    int sortListTime = sortList();
-    int sortSetTime = sortSet();
+    int sortVectorTime = sortVector(codesVector);
+    int sortListTime = sortList(codesList);
+    int sortSetTime = sortSet(codesSet);
 
     printRightJustified("Sort", 9);
-    printRightJustified(to_string(), 10); 
-    printRightJustified(to_string(), 10); 
-    printRightJustified(to_string(), 10); 
+    printRightJustified(to_string(sortVectorTime), 10); 
+    printRightJustified(to_string(sortListTime), 10); 
+    printRightJustified(to_string(sortSetTime), 10); 
     cout << endl;
 
     return 0;
@@ -134,11 +136,28 @@ int sortList(list<string> &codes)
 
 int sortSet(set<string> &codes)
 {
-    auto start = high_resolution_clock::now();
+    // no sorting is required for set because item is sorted by default so returning -1
+    return -1;
+}
 
-    // no sorting is required for set because item is sorted by default
+int insertVector(vector<string> &codesVector, string val)
+{
+    auto start = high_resolution_clock::now();
+    
+    codesVector.insert(codesVector.begin() + 20000, "TESTCODE");
+
     auto end = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(end - start);
 
     return duration.count() * 1000;
+}
+
+int insertList(list<string> &codesList, string val)
+{
+
+}
+
+int insertSet(set<string> &codesSet, string val)
+{
+
 }
