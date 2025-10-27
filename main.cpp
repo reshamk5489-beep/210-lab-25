eacb5af164ef1524f70c160ea34c9646753770f0
Resham <resha@MacBook-Air-2.local>
2025-10-26 21:23:37 -0700

    Implemented First Race Functions

--- main.cpp
+++ main.cpp
@@ -4,13 +4,16 @@
 #include <list>
 #include <set>
 using namespace std;
+using namespace std::chrono;
 
 int readCodesToVector();
 int readCodesToList();
 int readCodesToSet();
 
 int main() {
-    
+    readCodesToVector();
+    readCodesToList();
+    readCodesToSet();
 
     return 0;
 }
@@ -24,6 +27,8 @@ duration.count() references elapsed milliseconds
 
 int readCodesToVector()
 {
+    auto start = high_resolution_clock::now();
+
     ifstream fin("Codes.txt");
     vector<string> codes;
     string code;
@@ -35,11 +40,19 @@ int readCodesToVector()
 
     fin.close();
 
-    return 0;
+    cout << "Vector size: " << codes.size() << endl;
+    cout << "Vector[0]: " << codes[0] << endl;
+
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
+
+    return duration.count();
 }
 
 int readCodesToList()
 {
+    auto start = high_resolution_clock::now();
+
     ifstream fin("Codes.txt");
     list<string> codes;
     string code;
@@ -51,5 +64,33 @@ int readCodesToList()
 
     fin.close();
 
-    return 0;
+    cout << "List size: " << codes.size() << endl;
+
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
+
+    return duration.count();
+}
+
+int readCodesToSet()
+{
+    auto start = high_resolution_clock::now();
+
+    ifstream fin("Codes.txt");
+    set<string> codes;
+    string code;
+
+    while (fin >> code)
+    {
+        codes.insert(code);
+    }
+
+    fin.close();
+
+    cout << "Set size: " << codes.size() << endl;
+
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
+
+    return duration.count();
 }
