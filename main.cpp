a9eff7974804a9fd21458cb45762583793b89c5a
Resham <resha@MacBook-Air-2.local>
2025-10-26 21:13:22 -0700

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <fstream>
 #include <chrono>
+#include <list>
+#include <set>
 using namespace std;
+
+int readCodesToVector();
+int readCodesToList();
+int readCodesToSet();
 
 int main() {
     
@@ -14,3 +21,35 @@ auto end = high_resolution_clock::now()
 auto duration = duration_cast<milliseconds>(end - start)
 duration.count() references elapsed milliseconds
 */
+
+int readCodesToVector()
+{
+    ifstream fin("Codes.txt");
+    vector<string> codes;
+    string code;
+
+    while (fin >> code)
+    {
+        codes.push_back(code);
+    }
+
+    fin.close();
+
+    return 0;
+}
+
+int readCodesToList()
+{
+    ifstream fin("Codes.txt");
+    list<string> codes;
+    string code;
+
+    while (fin >> code)
+    {
+        codes.push_back(code);
+    }
+
+    fin.close();
+
+    return 0;
+}
